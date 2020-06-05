6c04f3bc6eed750c8a7323beaae5bdd761b4c85a
Henrique Maximiliano Macedo <hmaxmacedo@gmail.com>
2020-06-04 22:53:59 -0300

    Atualizado às 22:52 hs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const server = express()
 
+// configurar pasta pública
+server.use(express.static("public"))
+// configurar caminhos da conexão
+
+// configurar página principal
+// req = Requisição
+// res = Resposta
+server.get('/', (req, res)=> {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get('/create-point', (req, res)=> {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
 // ligando o servidor
-server.listen(3000)
+// server.listen(3000)
+server.listen(3001)
