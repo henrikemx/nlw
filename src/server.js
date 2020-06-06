a6109c8075e7fabe1bacaec30d40f809565ee2a1
henrikemx <hmaxmacedo@gmail.com>
2020-06-06 18:09:24 -0300

    Atualizados às 18:10 hs

--- src/server.js
+++ src/server.js
@@ -5,15 +5,26 @@ const server = express()
 server.use(express.static("public"))
 // configurar caminhos da conexão
 
+// utilizando template engine
+const nunjuncks = require('nunjucks')
+nunjuncks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
 // configurar página principal
 // req = Requisição
 // res = Resposta
 server.get('/', (req, res)=> {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: 'Seu marketplace de coleta de resíduos'})
 })
 
 server.get('/create-point', (req, res)=> {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get('/search', (req, res)=> {
+    return res.render("search-results.html")
 })
 // ligando o servidor
 // server.listen(3000)
