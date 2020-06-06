7ab37d04f96cf5b71f9c6dc2c7d5b5fd28e79c43
henrikemx <hmaxmacedo@gmail.com>
2020-06-05 23:19:10 -0300

    Atualizados às 23:19 hs

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,42 @@
-//importar a independência do sqlite3
-const sqlite3 = require('sqlite3').verbose
+const sqlite3 = require('sqlite3').verbose()
 
 // iniciar o objeto que irá fazer operações no db
 const db = new sqlite3.Database('./src/database/database.db')
+
+// utilizar o objeto de bd para nossas operações
+db.serialize(() => {
+    // criando uma tabela com comandos SQL
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+    // Inserir dados na tabela
+    const query = `
+    INSERT INTO places (
+        image, 
+        name, 
+        address, 
+        address2, 
+        state, 
+        city,
+        items
+    ) VALUES (
+        ?,?,?,?,?,?,?
+    );
+    `
+    db.run(query, [
+        "",
+    ])
+
+    // Consultar dados na tabela
+
+    //deletar dados na tabela
+})
